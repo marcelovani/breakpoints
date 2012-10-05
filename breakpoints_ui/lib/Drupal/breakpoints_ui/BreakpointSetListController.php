6215c3b76064cde4de609ab3365f6c2bdb597e3a
Peter Droogmans <peter@attiks.com>
2012-10-05 02:35:55 -0500

    Revert "breakpoints_ui removed"
    This reverts commit ab53c769c2de196d7a267999ad2e9c172f64138b.

---
+++ breakpoints_ui/lib/Drupal/breakpoints_ui/BreakpointSetListController.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\breakpoints_ui\BreakpointSetListController.
+ */
+
+namespace Drupal\breakpoints_ui;
+
+use Drupal\Core\Config\Entity\ConfigEntityListController;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\breakpoints\Breakpoint;
+
+/**
+ * Provides a listing of Breakpoint sets.
+ */
+class BreakpointSetListController extends ConfigEntityListController {
+
+  public function __construct($entity_type, $entity_info = FALSE) {
+    parent::__construct($entity_type, $entity_info);
+  }
+
+  /**
+   * Overrides Drupal\config\EntityListControllerBase::hookMenu();
+   */
+  public function hookMenu() {
+    $path = $this->entityInfo['list path'];
+    $items = parent::hookMenu();
+
+    // Override the access callback.
+    $items[$path]['title'] = 'Breakpoint sets';
+    $items[$path]['description'] = 'Manage list of breakpoint sets.';
+    $items[$path]['access callback'] = 'user_access';
+    $items[$path]['access arguments'] = array('administer breakpoints');
+
+    return $items;
+  }
+
+  /**
+   * Overrides Drupal\config\ConfigEntityListController::getOperations();
+   */
+  public function getOperations(EntityInterface $entity) {
+    $operations = parent::getOperations($entity);
+    $uri = $entity->uri();
+    $operations['duplicate'] = array(
+      'title' => t('Duplicate'),
+      'href' => $uri['path'] . '/duplicate',
+      'options' => $uri['options'],
+      'weight' => 15,
+    );
+    if ($entity->sourceType == Breakpoint::BREAKPOINTS_SOURCE_TYPE_THEME) {
+      if (!$entity->overridden) {
+        $operations['override'] = array(
+          'title' => t('Override'),
+          'href' => $uri['path'] . '/override',
+          'options' => $uri['options'],
+          'weight' => 15,
+        );
+      }
+      else {
+        $operations['revert'] = array(
+          'title' => t('Revert'),
+          'href' => $uri['path'] . '/revert',
+          'options' => $uri['options'],
+          'weight' => 15,
+        );
+      }
+    }
+    return $operations;
+  }
+
+}
